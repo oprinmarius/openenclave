681da36614d424153bf62315a32453138e48d1e2
vishnumohan1991 <vishnu.mohan@vvdntech.in>
2018-10-31 21:16:31 +0530

    Changes in crypto test

--- tests/crypto/enclave/host/host.c
+++ tests/crypto/enclave/host/host.c
@@ -3,13 +3,57 @@
 
 #include <assert.h>
 #include <limits.h>
+#include <fcntl.h>
 #include <openenclave/host.h>
 #include <openenclave/internal/error.h>
 #include <openenclave/internal/tests.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 #include "crypto_u.h"
+#include "../syscall_args.h"
+
+OE_OCALL void mbed_test_open(void* syscall_args)
+{
+    syscall_args_t* args = (syscall_args_t*)syscall_args;
+
+    args->fd = open(args->path, args->flags, args->mode);
+
+    return;
+}
+
+OE_OCALL void mbed_test_read(void* syscall_args)
+{
+    int ret;
+    syscall_args_t* args = (syscall_args_t*)syscall_args;
+
+    ret = read(args->fd, (char*)args->ptr, args->len);
+    args->ret = ret;
+
+    return;
+}
+
+OE_OCALL void mbed_test_readv(void* syscall_args)
+{
+    syscall_args_t* args = (syscall_args_t*)syscall_args;
+
+    args->ret = readv(args->fd, (const struct iovec*)args->ptr, args->len);
+
+    return;
+}
+
+OE_OCALL void mbed_test_close(void* syscall_args)
+{
+    syscall_args_t* args = (syscall_args_t*)syscall_args;
+
+    args->ret = close(args->fd);
+
+    return;
+}
 
 int main(int argc, const char* argv[])
 {
