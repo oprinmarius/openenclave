12f601724ac8ef2310f176e343fae76fc394a79a
Oprin Marius <moprin@cloudbasesolutions.com>
2020-04-01 19:52:08 +0300

    Change Windows 2019 sgx driver location
    Signed-off-by: Oprin Marius <moprin@cloudbasesolutions.com>

--- scripts/install-windows-prereqs.ps1
+++ scripts/install-windows-prereqs.ps1
@@ -548,12 +548,12 @@ function Install-DCAP-Dependencies {
             'WinServer2019' = @{
                 'sgx_base' = @{
                     'zip_path'    = "$PACKAGES_DIRECTORY\Intel_SGX_DCAP\Intel SGX DCAP for Windows *\LC_driver_${OS_VERSION}\Signed_*.zip"
-                    'location'    = 'root\SgxLCDevice'
+                    'location'    = 'acpi\int0e0c'
                     'description' = 'Intel(R) Software Guard Extensions Launch Configuration Service'
                 }
                 'sgx_dcap' = @{
                     'zip_path'    = "$PACKAGES_DIRECTORY\Intel_SGX_DCAP\Intel SGX DCAP for Windows *\DCAP_INF\${OS_VERSION}\Signed_*.zip"
-                    'location'    = 'root\SgxLCDevice_DCAP'
+                    'location'    = 'SWC\VEN_INT&DEV_0E0C_DCAP'
                     'description' = 'Intel(R) Software Guard Extensions DCAP Components Device'
                 }
             }
@@ -589,7 +589,7 @@ function Install-DCAP-Dependencies {
                     Remove-DCAPDriver -Name $drivers[${OS_VERSION}][$driver]['location']
                 }
             }
-            if ($LaunchConfiguration -eq "SGX1FLC")
+            if (($LaunchConfiguration -eq "SGX1FLC") -or (${OS_VERSION} -eq "WinServer2019"))
             {
                 Write-Output "Installing driver $($drivers[${OS_VERSION}][$driver]['location'])"
                 $install = & $devConBinaryPath install "$($inf.FullName)" $drivers[${OS_VERSION}][$driver]['location']
